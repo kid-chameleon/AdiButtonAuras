ccae5dfb21df7c21258f22f0f4d3e409096deeb1
Mike <56284637+kid-chameleon@users.noreply.github.com>
2024-09-26 09:15:56 -0700

    fix

--- rules/UserRules.lua
+++ rules/UserRules.lua
@@ -1,10 +1,6 @@
 --[[
 AdiButtonAuras - Display auras on action buttons.
-<<<<<<< HEAD
-Copyright 2013-2021 Adirelle (adirelle@gmail.com)
-=======
 Copyright 2013-2023 Adirelle (adirelle@gmail.com)
->>>>>>> upstream/master
 All rights reserved.
 
 This file is part of AdiButtonAuras.
@@ -107,7 +103,7 @@ function addon:CompileUserRules()
 		end
 	end
 
-	for i = count+1, #builders do
+	for i = count + 1, #builders do
 		builders[i] = nil
 	end
 
